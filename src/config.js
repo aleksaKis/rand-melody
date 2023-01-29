afd816c66c2c97561d67f3a1eb2dda99c49b093c
Aleksa Kis <kishaleksa@gmail.com>
2023-01-29 17:15:40 +0100

    feat: play-pause

--- src/config.js
+++ src/config.js
@@ -1,11 +1,16 @@
-export const NOTE_CONFIGURATION = {
+export const noteConfig = {
   width: 20,
   border: "#bbb",
   highlight: "#0cc",
   defaultColor: "#333333",
+  playedClass: "highlight",
+  triggerDelay: 160, // ms
 };
 
 export const GRID_CONFIGURATION = {
   columns: 64,
-  rows: 24,
+  octaves: 3, // 1 to 3
+  getRows() {
+    return 8 * this.octaves;
+  },
 };
--- src/index.js
+++ src/index.js
@@ -1,15 +1,45 @@
 import { sleep } from "./utils.js";
-import { NOTE_CONFIGURATION, GRID_CONFIGURATION } from "./config.js";
+import { noteConfig, GRID_CONFIGURATION } from "./config.js";
 
-const PLAY_RELEASE = 105; // ms
+const PLAY_KEYWORD = "play";
+const PAUSE_KEYWORD = "pause";
+
 const grid = document.getElementById("grid");
-
 grid.style.display = "grid";
-grid.style.gridTemplateColumns = `repeat(${GRID_CONFIGURATION.columns}, ${NOTE_CONFIGURATION.width}px)`;
-grid.style.gridTemplateRows = `repeat(${GRID_CONFIGURATION.rows}, ${NOTE_CONFIGURATION.width}px)`;
+grid.style.gridTemplateColumns = `repeat(${GRID_CONFIGURATION.columns}, ${noteConfig.width}px)`;
+grid.style.gridTemplateRows = `repeat(${GRID_CONFIGURATION.getRows()}, ${
+  noteConfig.width
+}px)`;
 
-function play() {
-  startLink().then();
+document.addEventListener("keydown", (event) => {
+  if (event.code === "Space") {
+    togglePlay();
+  }
+});
+
+document.getElementById("play").addEventListener("click", togglePlay);
+buildGrid();
+
+function togglePlay() {
+  const playButton = document.getElementById("play");
+  if (playButton.textContent === PLAY_KEYWORD) handlePlay();
+  else handlePause();
+}
+
+function handlePlay() {
+  document.getElementById("play").textContent = PAUSE_KEYWORD;
+  clearGrid();
+  startLink().then(handlePause);
+}
+
+function handlePause() {
+  document.getElementById("play").textContent = PLAY_KEYWORD;
+}
+
+function clearGrid() {
+  Array.from(document.getElementsByClassName(noteConfig.highlight)).forEach(
+    (el) => (el.style.backgroundColor = noteConfig.defaultColor)
+  );
 }
 
 function getItemName(index) {
@@ -19,27 +49,30 @@ function getItemName(index) {
 function buildGrid() {
   for (
     let i = 0;
-    i < GRID_CONFIGURATION.rows * GRID_CONFIGURATION.columns;
+    i < GRID_CONFIGURATION.getRows() * GRID_CONFIGURATION.columns;
     i++
   ) {
     const item = document.createElement("div");
     item.id = getItemName(i);
-    item.style.border = `1px solid ${NOTE_CONFIGURATION.border}`;
-    item.style.backgroundColor = NOTE_CONFIGURATION.defaultColor;
+    item.style.border = `1px solid ${noteConfig.border}`;
+    item.style.backgroundColor = noteConfig.defaultColor;
     grid.append(item);
   }
 }
 
 function generateSequence() {
   const sequence = Array(GRID_CONFIGURATION.columns);
-  for (let i = 0; i < GRID_CONFIGURATION.columns; i++) {
-    if (i % 3 === 0 && i % 5 === 0) {
-      sequence[i] = null;
-    } else {
-      sequence[i] = Math.ceil(Math.random() * GRID_CONFIGURATION.rows);
-    }
-  }
+  for (let i = 0; i < GRID_CONFIGURATION.columns; i++)
+    sequence[i] = getSequenceValue(i);
   return sequence;
+}
+
+function getSequenceValue(i) {
+  console.log(i);
+  // add swing
+  if (i !== 0 && i % 7 === 0 && i % 3 === 0) return null;
+
+  return Math.ceil(Math.random() * GRID_CONFIGURATION.getRows());
 }
 
 function getNotePosition(coords) {
@@ -49,25 +82,37 @@ function getNotePosition(coords) {
 
 async function playNote(coords) {
   const note = document.getElementById(getItemName(getNotePosition(coords)));
-  note.style.backgroundColor = NOTE_CONFIGURATION.highlight;
+
+  note.classList = noteConfig.highlight;
+  note.style.backgroundColor = noteConfig.highlight;
+
   const octave = Math.ceil(coords[0] / 8) + 2; // octaves are from 3 to 5
-  const tone = Math.ceil(coords[0] / 3);
+  const tone = Math.ceil(coords[0] / GRID_CONFIGURATION.octaves);
   await playSound(getToneName(tone) + octave);
 }
 
 async function playSound(note) {
   const noteAudio = new Audio(`./data/notes/${note}.mp3`);
-  await noteAudio.play(console.log.bind(this));
+  await noteAudio.play();
+  noteAudio.remove();
 }
 
 async function startLink() {
   const sequence = generateSequence();
   for (let i = 0; i < sequence.length; i++) {
+    // Handle pause
+    if (document.getElementById("play").textContent === PLAY_KEYWORD) break;
+
     if (sequence[i]) playNote([sequence[i], i]);
-    await sleep(PLAY_RELEASE);
+    await sleep(noteConfig.triggerDelay);
   }
 }
 
+/**
+ * Finds a note based on it's position in scale.
+ * @param {number} index
+ * @returns a minor scale note
+ */
 function getToneName(index) {
   switch (index) {
     case 1:
@@ -86,14 +131,7 @@ function getToneName(index) {
       return "g";
     case 8:
       return "a";
+    default:
+      return "c";
   }
 }
-
-document.addEventListener("keydown", (event) => {
-  if (event.code === "Space") {
-    play();
-  }
-});
-
-document.getElementById("play").addEventListener("click", play);
-buildGrid();

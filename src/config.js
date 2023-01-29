8f3132900973eba9bff7af559b07418c3d21f548
Aleksa Kis <kishaleksa@gmail.com>
2023-01-29 17:15:40 +0100

    feat: Add notes

---
+++ src/config.js
@@ -0,0 +1,11 @@
+export const NOTE_CONFIGURATION = {
+  width: 20,
+  border: "#bbb",
+  highlight: "#0cc",
+  defaultColor: "#333333",
+};
+
+export const GRID_CONFIGURATION = {
+  columns: 64,
+  rows: 24,
+};
--- src/index.js
+++ src/index.js
@@ -1,58 +1,99 @@
-"use-strict";
+import { sleep } from "./utils.js";
+import { NOTE_CONFIGURATION, GRID_CONFIGURATION } from "./config.js";
 
-const NUMBER_OF_COLUMNS = 32;
-const NUMBER_OF_ROWS = 8;
-const ITEM_WIDTH = 20;
-const ITEM_BORDER_COLOR = "#bbb";
-const ITEM_HIGHLIGHTED = "#0cc";
-const ITEM_DEFAULT_BACKGROUND = "transparent";
-const PLAY_RELEASE = 2000; // ms
+const PLAY_RELEASE = 105; // ms
 const grid = document.getElementById("grid");
 
 grid.style.display = "grid";
-grid.style.gridTemplateColumns = `repeat(${NUMBER_OF_COLUMNS}, ${ITEM_WIDTH}px)`;
-grid.style.gridTemplateRows = `repeat(${NUMBER_OF_ROWS}, ${ITEM_WIDTH}px)`;
+grid.style.gridTemplateColumns = `repeat(${GRID_CONFIGURATION.columns}, ${NOTE_CONFIGURATION.width}px)`;
+grid.style.gridTemplateRows = `repeat(${GRID_CONFIGURATION.rows}, ${NOTE_CONFIGURATION.width}px)`;
 
-const getItemName = (index) => {
-  return "ITEM_" + index;
-};
+function play() {
+  startLink().then();
+}
 
-const buildGrid = () => {
-  for (let i = 0; i < NUMBER_OF_ROWS * NUMBER_OF_COLUMNS; i++) {
+function getItemName(index) {
+  return "NOTE_" + index;
+}
+
+function buildGrid() {
+  for (
+    let i = 0;
+    i < GRID_CONFIGURATION.rows * GRID_CONFIGURATION.columns;
+    i++
+  ) {
     const item = document.createElement("div");
     item.id = getItemName(i);
-    item.style.border = `1px solid ${ITEM_BORDER_COLOR}`;
+    item.style.border = `1px solid ${NOTE_CONFIGURATION.border}`;
+    item.style.backgroundColor = NOTE_CONFIGURATION.defaultColor;
     grid.append(item);
   }
-};
+}
 
-const generateSequence = () => {
-  const sequence = Array(NUMBER_OF_COLUMNS);
-  for (let i = 0; i < NUMBER_OF_COLUMNS; i++) {
-    sequence[i] = Math.ceil(Math.random() * NUMBER_OF_ROWS);
+function generateSequence() {
+  const sequence = Array(GRID_CONFIGURATION.columns);
+  for (let i = 0; i < GRID_CONFIGURATION.columns; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      sequence[i] = null;
+    } else {
+      sequence[i] = Math.ceil(Math.random() * GRID_CONFIGURATION.rows);
+    }
   }
   return sequence;
-};
+}
 
 function getNotePosition(coords) {
   const [x, y] = coords;
-  return y + (x - 1) * NUMBER_OF_COLUMNS;
+  return y + (x - 1) * GRID_CONFIGURATION.columns;
 }
 
-const playNote = async (coords) => {
+async function playNote(coords) {
   const note = document.getElementById(getItemName(getNotePosition(coords)));
-  note.style.backgroundColor = ITEM_HIGHLIGHTED;
-  return setTimeout(() => {
-    note.style.backgroundColor = ITEM_DEFAULT_BACKGROUND;
-  }, PLAY_RELEASE);
-};
+  note.style.backgroundColor = NOTE_CONFIGURATION.highlight;
+  const octave = Math.ceil(coords[0] / 8) + 2; // octaves are from 3 to 5
+  const tone = Math.ceil(coords[0] / 3);
+  await playSound(getToneName(tone) + octave);
+}
 
-const startLink = async () => {
+async function playSound(note) {
+  const noteAudio = new Audio(`./data/notes/${note}.mp3`);
+  await noteAudio.play(console.log.bind(this));
+}
+
+async function startLink() {
   const sequence = generateSequence();
   for (let i = 0; i < sequence.length; i++) {
-    await playNote([sequence[i], i]);
+    if (sequence[i]) playNote([sequence[i], i]);
+    await sleep(PLAY_RELEASE);
   }
-};
+}
 
+function getToneName(index) {
+  switch (index) {
+    case 1:
+      return "a";
+    case 2:
+      return "b";
+    case 3:
+      return "c";
+    case 4:
+      return "d";
+    case 5:
+      return "e";
+    case 6:
+      return "f";
+    case 7:
+      return "g";
+    case 8:
+      return "a";
+  }
+}
+
+document.addEventListener("keydown", (event) => {
+  if (event.code === "Space") {
+    play();
+  }
+});
+
+document.getElementById("play").addEventListener("click", play);
 buildGrid();
-startLink().then();
